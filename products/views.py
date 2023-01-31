5ce00d236c7c42420f9e6b23eb8f809bd058dd98
alexmao <sashok111293@gmail.com>
2023-01-31 22:31:31 +0300

    add models

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from products.models import ProductCategory, Product
 
 # Create your views here.
 
@@ -11,5 +11,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
