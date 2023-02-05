e668722d1ffe1eae20d8ecce80d2224b1f2bcad4
alexmao <sashok111293@gmail.com>
2023-02-05 19:30:48 +0300

    add registration form

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from products.models import ProductCategory, Product
+from users.models import User
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(User)
